2470b7eaac6b95e165843ba9bac4e6fd866aa4cf
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-12-04 11:08:20 +0100

    Update `USER_AGENT_NAME` to use `CARGO_PKG_NAME`

--- src/constant.rs
+++ src/constant.rs
@@ -1,5 +1,7 @@
 pub(crate) const API_ROOT_URL: &str = "https://api.strike.me/v1";
 
-pub(crate) const USER_AGENT_NAME: &str = concat!("strike-api/", env!("CARGO_PKG_VERSION"));
+/// User Agent for the client
+pub(super) const USER_AGENT_NAME: &str =
+    concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
 pub(crate) const BTC_TICKER: &str = "BTC";
